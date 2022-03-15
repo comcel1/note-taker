c263e45d79ec51f5356b775a504670cabb0f206d
PaulNelsonOld <90969624+comcel1@users.noreply.github.com>
2022-03-15 17:38:58 -0500

    added get and post routes for notes

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const htmlRoutes = require("./routes/htmlRoutes/index");
+const apiRoutes = require("./routes/apiRoutes/index");
 
 // parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
@@ -10,6 +11,7 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 
 // Routes
+app.use("/api", apiRoutes);
 app.use("/", htmlRoutes);
 
 // Server
